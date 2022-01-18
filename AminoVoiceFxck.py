19f92598bded2632d267b1f80b22cb079fbd54c3
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-01-18 13:57:27 +0600

    Update AminoVoiceFxck.py

--- AminoVoiceFxck.py
+++ AminoVoiceFxck.py
@@ -1,31 +1,26 @@
-import amino
-import asyncio
-import pyfiglet
-import rainbowtext
-print(rainbowtext.text("""Script by Lil Zevi
-Github : https://github.com/LilZevi"""))
-print(rainbowtext.text(pyfiglet.figlet_format("Aminovoicefxck", font="small")))
+import samino
+from rainbowtext import text
+from pyfiglet import figlet_format
+from websocket import WebSocketConnectionClosedException
+print(text("""Script by deluvsushi
+Github : https://github.com/deluvsushi"""))
+print(text(figlet_format("aminovoicefxck", font="small")))
 
-async def main():
-	client = amino.Client()
-	emails = open("emails.txt", "r")
-	chatlink = input(rainbowtext.text("Chat Link >> "))
-	password = input(rainbowtext.text("Password For All Accounts >> "))
-	for line in emails:
-		email = line.strip()
-		try:
-			await client.login(email=email, password=password)
-			chat_info = await client.get_from_code(chatlink)
-			chat_id = chat_info.objectId
-			community_id = chat_info.path[1:chat_info.path.index('/')]
-			await client.join_voice_chat(comId=community_id, chatId=chat_id, joinType=2)
-			print(rainbowtext.text(f"{email} Joined To Voice Chat"))
-		except amino.lib.util.exceptions.VerificationRequired as e:
-			print(f"{email} VerificationRequired")
-			link = e.args[0]['url']
-			print(link)
-		except amino.lib.util.exceptions.ActionNotAllowed:
-			print(rainbowtext.text(f"ActionNotAllowed {email}"))
-		except:
-			print(rainbowtext.text(f"{email} Can't Join To Voice Chat"))
-			pass
+def main():
+	client = samino.Client()
+	password = input(text("-- Password for all accounts::: "))
+	link_Info = client.get_from_link(input(text("-- Chat Link::: ")))
+	com_id = linkInfo.comId; chat_id = linkInfo.objectId
+	with open ("emails.txt", "r") as emails:
+		for line in emails:
+			try:
+				email = line.strip()
+				client.login(email=email, password=password)
+				local = samino.Local(comId=com_id)
+				local.join_chat(chatId=chat_id)
+				client.socketClient.joinVideoChatAsSpectator(com_id, chat_id)
+				print(text(f"-- {email} Joined to voice chat..."))
+			except (samino.lib.Except, WebSocketConnectionClosedException) as e:
+				print(f"- Error::: {email} - {e.args[0]}\n")
+
+main()
