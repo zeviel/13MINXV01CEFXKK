e3790ae2c695653dc5fbec2d0ac0648581df8243
zeviel <77536370+zeviel@users.noreply.github.com>
2022-08-25 15:28:25 +0800

    Update 13MINXV01CEFXKK.py

--- 13MINXV01CEFXKK.py
+++ 13MINXV01CEFXKK.py
@@ -1,26 +1,23 @@
-import samino
+import amino
 from rainbowtext import text
 from pyfiglet import figlet_format
-from websocket import WebSocketConnectionClosedException
-print(text("""Script by deluvsushi
-Github : https://github.com/deluvsushi"""))
+print(text("""Script by zeviel
+Github : https://github.com/zeviel"""))
 print(text(figlet_format("13MINXV01CEFXKK", font="small")))
 
-def main():
-	client = samino.Client()
-	password = input(text("-- Password for all accounts::: "))
-	link_Info = client.get_from_link(input(text("-- Chat Link::: ")))
-	com_id = linkInfo.comId; chat_id = linkInfo.objectId
-	with open ("emails.txt", "r") as emails:
-		for line in emails:
-			try:
-				email = line.strip()
-				client.login(email=email, password=password)
-				local = samino.Local(comId=com_id)
-				local.join_chat(chatId=chat_id)
-				client.socketClient.joinVideoChatAsSpectator(com_id, chat_id)
-				print(text(f"-- {email} Joined to voice chat..."))
-			except (samino.lib.Except, WebSocketConnectionClosedException) as e:
-				print(f"- Error::: {email} - {e.args[0]}\n")
-
-main()
+client = amino.Client()
+password = input(text("-- Password for all accounts::: "))
+link_info = client.get_from_code(input(text("-- Chat Link::: ")))
+com_id = link_info.comId
+chat_id = link_info.objectId
+with open ("emails.txt", "r") as file:
+	for line in file:
+		try:
+			email = line.strip()
+			client.login(email=email, password=password)
+			sub_client = amino.SubClient(comId=com_id, profile=client.profile)
+			sub_client.join_chat(chatId=chat_id)
+			client.join_video_chat_as_viewer(comId=com_id, chatId=chat_id)
+			print(text(f"-- {email} Joined to voice chat..."))
+		except Exception as e:
+			print(e)
